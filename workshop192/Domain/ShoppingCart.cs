460f7ef83675392d485c8e60703ce2d8467de3ee
barvak <barvak98@gmail.com>
2019-04-01 18:57:55 +0300

    added functions in Store & product Classes

--- workshop192/Domain/ShoppingCart.cs
+++ workshop192/Domain/ShoppingCart.cs
@@ -8,5 +8,24 @@ namespace workshop192.Domain
 {
     class ShoppingCart
     {
+        private LinkedList<Product> productList;
+
+        public ShoppingCart()
+        {
+            productList = new LinkedList<Product>();
+        }
+
+        public String addToCart(Product product, int amount)
+        {
+            int quantity = product.getQuantityLeft();
+            if ( quantity - amount> 0)
+            {
+                product.setQuantityLeft(quantity - amount);
+                return "";
+            }
+
+            return "- product quantity";
+
+        }
     }
 }
--- workshop192/Domain/Store.cs
+++ workshop192/Domain/Store.cs
@@ -8,5 +8,75 @@ namespace workshop192.Domain
 {
     class Store
     {
+        public int storeID;
+        public string storeName;
+        public string description;
+        public LinkedList<Product> productList;
+
+        public Store (int id, string storeName, string description)
+        {
+            this.storeID = id;
+            this.storeName = storeName;
+            this.description = description;
+            productList = new LinkedList<Product>();
+        }
+
+        public void addProduct(Product p)
+        {
+            productList.AddFirst(p);
+        }
+        public void removeProduct(Product p)
+        {
+            productList.Remove(p);
+        }
+        public bool productExists(int productID)
+        {
+           foreach (Product p  in productList)
+            {
+                if (p.getProductID() == productID)
+                    return true;
+            }
+            return false;
+        }
+
+        public LinkedList<Product> getProductList()
+        {
+            return this.productList;
+        }
+
+        public int getStoreID()
+        {
+            return this.storeID;
+        }
+        public void setStoreID(int id)
+        {
+            this.storeID = id;
+        }
+
+        public String getStoreName()
+        {
+            return this.storeName;
+        }
+
+        public void setStoreName(String storeName)
+        {
+            this.storeName = storeName;
+        }
+
+        public String getDescription()
+        {
+            return this.description;
+        }
+
+        public void setDescription(String description)
+        {
+            this.description = description;
+        }
+        
+
+        
+
+        // public ? getPurchaseHistory (){} ------------------
+
     }
 }
