2cf8b6f712ea352eb639b40e7ab8a7fd4a94a4bc
etay <etay21@gmail.com>
2019-04-02 10:58:35 +0300

    merge with etay

---
+++ workshop192/Domain/LoggedIn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop192.Domain
+{
+    class LoggedIn : UserState
+    {
+        public string closeStore(int id)
+        {
+            return "ERROR: not an admin";
+        }
+
+        public string createStore()
+        {
+            return "ERROR: not an admin";
+        }
+
+        public string getPurchaseHistory(SubscribedUser sub)
+        {
+            return DBSubscribedUser.getInstance().getPurchaseHistory(sub);
+        }
+
+        public string login(string username, string password, Session session)
+        {
+            return "ERROR: User already logged in";
+        }
+
+        public string logout(SubscribedUser sub)
+        {
+            return DBSubscribedUser.getInstance().logout(sub);
+        }
+
+        public string register(string username, string password)
+        {
+            return "ERROR: User already registered";
+        }
+
+        public string removeUser(string username)
+        {
+            return "ERROR: not an admin";
+        }
+    }
+}
