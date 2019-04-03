d263ae23014ce0e85e066d910741da7f37778b28
etay <etay21@gmail.com>
2019-04-03 17:10:53 +0300

    after update of visual

--- workshop192Tests/Domain/DBSessionTests.cs
+++ workshop192Tests/Domain/DBSessionTests.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
 using workshop192.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace workshop192.Domain.Tests
 {
