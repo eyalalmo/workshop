975531fd7b7c9c175e3942bfb774d150379d50d6
etay <etay21@gmail.com>
2019-04-04 20:42:00 +0300

    at 4.4

--- UnitTestProject2/StoreServiceTests.cs
+++ UnitTestProject2/StoreServiceTests.cs
@@ -12,6 +12,9 @@ namespace workshop192.ServiceLayer.Tests
     [TestClass()]
     public class StoreServiceTests
     {
+
+       
+
         //3.2
         [TestMethod()]
         public void TestMethod1()
@@ -77,8 +80,100 @@ namespace workshop192.ServiceLayer.Tests
             Store store = storeService.addStore("myStore", "the best store ever", session1);
             storeService.addManager(store, "dani", true, true, true, session1);
             Assert.AreNotEqual(storeService.addManager(store, "dani", true, true, true, session1), "");
+
         }
 
-       
+       // 4.4
+        [TestMethod()]
+        public void removeOwnerTest()
+        {
+            StoreService storeService = StoreService.getInstance();
+            UserService userService = UserService.getInstance();
+            Session session1 = new Session();
+            Session session2 = new Session();
+            Session session3 = new Session();
+            Session session4 = new Session();
+
+            userService.register(session1, "anna", "banana"); //first owner
+            userService.login(session1, "anna", "banana");
+        
+            userService.register(session2, "dani1", "123");
+            userService.login(session2, "dani1", "123");
+
+            userService.register(session3, "dani2", "123");
+            userService.register(session4, "dani3", "123");
+
+            Store store = storeService.addStore("myStore", "the best store ever", session1);
+            Assert.AreEqual("", storeService.addOwner(store, "dani1", session1));
+            Assert.AreEqual("", storeService.addOwner(store, "dani2", session2));
+            Assert.AreEqual("", storeService.addOwner(store, "dani3", session2));
+            Assert.AreEqual(storeService.removeRole(store, "dani1", session1),"");
+            foreach (StoreRole role in store.getRoles())
+            {
+                if(role.getAppointedBy()==session1.getSubscribedUser())
+                {
+                    Assert.Fail();
+                 }
+            }
+    
+        }
+        // 4.4
+        [TestMethod()]
+        public void removeOwnerTestFaild1()
+        {
+            StoreService storeService = StoreService.getInstance();
+            UserService userService = UserService.getInstance();
+            Session session1 = new Session();
+            Session session2 = new Session();
+            Session session3 = new Session();
+            Session session4 = new Session();
+
+            userService.register(session1, "anna", "banana"); //first owner
+            userService.login(session1, "anna", "banana");
+
+            userService.register(session2, "dani1", "123");
+            userService.login(session2, "dani1", "123");
+
+            userService.register(session3, "dani2", "123");
+            userService.register(session4, "dani3", "123");
+
+            Store store = storeService.addStore("myStore", "the best store ever", session1);
+            Assert.AreEqual("", storeService.addOwner(store, "dani1", session1));
+            Assert.AreEqual("", storeService.addOwner(store, "dani2", session2));
+            Assert.AreEqual("", storeService.addOwner(store, "dani3", session2));
+            Assert.AreNotEqual(storeService.removeRole(store, "dani1", session2), "");
+        
+        }
+
+        // 4.4
+        [TestMethod()]
+        public void removeOwnerTestFaild2()
+        {
+            StoreService storeService = StoreService.getInstance();
+            UserService userService = UserService.getInstance();
+            Session session1 = new Session();
+            Session session2 = new Session();
+            Session session3 = new Session();
+            Session session4 = new Session();
+
+            userService.register(session1, "anna", "banana"); //first owner
+            userService.login(session1, "anna", "banana");
+
+            userService.register(session2, "dani1", "123");
+            userService.login(session2, "dani1", "123");
+
+            userService.register(session3, "dani2", "123");
+            userService.register(session4, "dani3", "123");
+
+            Store store = storeService.addStore("myStore", "the best store ever", session1);
+            Assert.AreEqual("", storeService.addOwner(store, "dani1", session1));
+            Assert.AreEqual("", storeService.addOwner(store, "dani2", session2));
+            Assert.AreNotEqual(storeService.removeRole(store, "dani3", session2), "");
+        }
+
+
+
+
+
     }
 }
