821140ae7df7950f42ed75904d4ca6c44fba86ec
etay <etay21@gmail.com>
2019-04-04 19:53:28 +0300

    aa

--- UnitTestProject2/StoreServiceTests.cs
+++ UnitTestProject2/StoreServiceTests.cs
@@ -25,24 +25,6 @@ namespace workshop192.ServiceLayer.Tests
            // Store store = 
         }
 
-        //4.4
-        [TestMethod()]
-        public void addMannagerByAnOwner3()
-        {
-            StoreService storeService = StoreService.getInstance();
-            UserService userService = UserService.getInstance();
-            Session session1 = new Session();
-            Session session2 = new Session();
-
-            userService.register(session1, "anna", "banana");
-            userService.login(session1, "anna", "banana");
-
-            userService.register(session2, "dani", "123");
-
-            Store store = storeService.addStore("myStore", "the best store ever", session1);
-            storeService.addManager(store, "dani", true, true, true, session1);
-            Assert.AreNotEqual(storeService.addManager(store, "dani", true, true, true, session1), "");
-        }
-
+       
     }
 }
