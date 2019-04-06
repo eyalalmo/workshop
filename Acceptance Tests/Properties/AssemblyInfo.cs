79e057a547cbd33ade4a7ca5aa1303531459abe6
etay21 <etay21@gmail.com>
2019-04-06 14:58:53 +0300

    Reorganize

--- Acceptance Tests/Properties/AssemblyInfo.cs
+++ Acceptance Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("UnitTestProject2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("UnitTestProject2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("db70287a-e8fd-48b6-901f-e7fddb8d2c1f")]
+[assembly: Guid("7a7a05f4-a426-478a-a99a-b0eb77c42bdc")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
