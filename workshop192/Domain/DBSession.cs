291d4274ba6d18e0a2369d0aae2b00a3ffef1031
eyal <eyalalmo@post.bgu.ac.il>
2019-04-08 17:20:08 +0300

    DB files exceptions throwing

--- workshop192/Domain/DBSession.cs
+++ workshop192/Domain/DBSession.cs
@@ -27,19 +27,17 @@ namespace workshop192.Domain
         {
             sessions = new LinkedList<Session>();
         }
-        public String addSession(Session s)
+        public void addSession(Session s)
         {
             if (sessions.Contains(s))
-                return "ERROR: Session already exists";
+                throw new AlreadyExistException("session already exists");
             sessions.AddFirst(s);
-            return "";
         }
-        public String removeSession(Session s)
+        public void removeSession(Session s)
         {
             if (!sessions.Contains(s))
-                return "ERROR: Session does not exist, cannot remove it";
+                throw new DoesntExistException("session does not exist, can't remove it");
             sessions.Remove(s);
-            return "";
         }
         public Session getSessionOfSubscribedUser(SubscribedUser sub)
         {
@@ -52,11 +50,9 @@ namespace workshop192.Domain
             }
             return null;
         }
-        public String getSession(Session s)
+        public bool sessionExists(Session s)
         {
-            if (sessions.Contains(s))
-                return "";
-            return "error: session isnt added";
+            return sessions.Contains(s);
         }
         public void initSession()
         {
---
+++ workshop192/Exceptions/StoreException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace workshop192.Domain
+{
+    [Serializable]
+    internal class StoreException : Exception
+    {
+        public StoreException()
+        {
+        }
+
+        public StoreException(string message) : base(message)
+        {
+        }
+
+        public StoreException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StoreException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
