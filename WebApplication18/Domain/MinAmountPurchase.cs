d3abcf0c98f1a35e93d0af743f6f2e95b9303d40
Bar Vaknin <barvak@post.bgu.ac.il>
2019-05-07 12:48:06 +0300

    added Min amount Purchase policy and Max amount Purchase policy

---
+++ WebApplication18/Domain/MinAmountPurchase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using workshop192.Domain;
+
+namespace WebApplication18.Domain
+{
+    public class MinAmountPurchase : PurchasePolicy
+    {
+        
+    }
+}
