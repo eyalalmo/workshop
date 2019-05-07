5262d42c371772fad0d940a24de42fd34afb7e82
yaelkach <yaelkach@post.bgu.ac.il>
2019-05-07 15:32:31 +0300

    itay merge

--- WebApplication18/Controllers/UesrController.cs
+++ WebApplication18/Controllers/UesrController.cs
@@ -19,7 +19,7 @@ namespace WebApplication18.Controllers
         public string register(String Username, String Password)
         {
 
-            int session = UserService.getInstance().getUserByHash(System.Web.HttpContext.Current.Request.Cookies["HashCode"].Value);
+            Session session = UserService.getUserByHash(System.Web.HttpContext.Current.Request.Cookies["HashCode"].Value);
             UserService.getInstance().register(session, Username, Password);
  
            return "ok";
@@ -32,21 +32,12 @@ namespace WebApplication18.Controllers
         public Object login(String Username, String Password)
         {
             //Session session = UserService.getInstance().startSession();
-            int session = UserService.getInstance().getUserByHash(System.Web.HttpContext.Current.Request.Cookies["HashCode"].Value);
+            Session session = UserService.getUserByHash(System.Web.HttpContext.Current.Request.Cookies["HashCode"].Value);
             UserService.getInstance().login(session, Username, Password);
            //String hash = System.Web.HttpContext.Current.Request.Cookies["HashCode"].Value;
            // UserService.addUser(hash, session);
             return "ok";
         }
-        [Route("api/user/getAllProducts")]
-        [HttpGet]
-        public string getAllProducts(String Username, String Password)
-        {
-
-            //LinkedList<Product> list = UserService.getInstance().getAllProducts();
-            //HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, list);
-            //return response;
-            return "ok";
 
 
         }
@@ -89,4 +80,4 @@ namespace WebApplication18.Controllers
 
 
     }
-}
+
