f81389f5b6265c43cdba8ff8fb72fc0f826def6a
eyal <eyalalmo@post.bgu.ac.il>
2019-06-15 22:32:44 +0300

    test for policy

--- WebApplication18/Domain/DBSubscribedUser.cs
+++ WebApplication18/Domain/DBSubscribedUser.cs
@@ -103,11 +103,11 @@ namespace workshop192.Domain
                 lock (connection)
                 {
                     connection.Open();
-                        connection.Execute("DELETE FROM Register");
-                        connection.Execute("DELETE FROM BasketCart");
-                        connection.Execute("DELETE FROM CartProduct");
-                        instance = new DBSubscribedUser();
-                        connection.Close();
+                    connection.Execute("DELETE FROM Register");
+                    connection.Execute("DELETE FROM BasketCart");
+                    connection.Execute("DELETE FROM CartProduct");
+                    instance = new DBSubscribedUser();
+                    connection.Close();
                 }
 
             }
