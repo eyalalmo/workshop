04f0f01a302ef1fd585519882a11c787eb30d0e7
yaelkach <yaelkach@post.bgu.ac.il>
2019-06-15 11:26:59 +0300

    changes

--- workshop192Tests/Domain/DBDiscountTests.cs
+++ workshop192Tests/Domain/DBDiscountTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using workshop192.Domain;
 using workshop192.ServiceLayer;
@@ -15,6 +16,7 @@ namespace UnitTests.Domain
             private UserService userService = UserService.getInstance();
 
             Store store1;
+            Store store2;
             Product p1;
 
             Product bamba;
@@ -27,7 +29,7 @@ namespace UnitTests.Domain
                 //DBProduct.getInstance().initTests();
                 //DBStore.getInstance().initTests();
                 store1 = new Store("Makolet", "groceryStore");
-               
+                store2 = new Store("Apple", "apples");
                 p1 = new Product("shirt", "clothing", 50, 4, 4, store1);
                 store1.addProduct(p1);
                 bamba = new Product("bamba", "food", 15, 5, 17, store1);
@@ -36,7 +38,7 @@ namespace UnitTests.Domain
                 store1.addProduct(bisli);
             }
             [TestMethod]
-            public void AddDiscount()
+            public void AddDiscountSuccess()
             {
                 try
                 {
@@ -44,6 +46,58 @@ namespace UnitTests.Domain
                     dbdiscount.addDiscount(v);
                     DiscountComponent d = dbdiscount.getDiscountByID(v.getId());
                     Assert.IsTrue(d.getId() == v.getId());
+
+                }
+                catch (Exception)
+                {
+                    Assert.Fail();
+                }
+
+            }
+            [TestMethod]
+            public void AddDiscountFail()
+            {
+                try
+                {
+                    VisibleDiscount v = new VisibleDiscount(0.2, "12/12/2017", "StoreVisibleDiscount", store1.getStoreID());
+                    Assert.Fail();
+
+                }
+                catch (Exception)
+                {
+                    Assert.IsTrue(true);
+                }
+
+            }
+            [TestMethod]
+            public void RemoveDiscount()
+            {
+                try
+                {
+                    VisibleDiscount v = new VisibleDiscount(0.2, "12/12/2020", "StoreVisibleDiscount", store1.getStoreID());
+                    dbdiscount.addDiscount(v);
+                    dbdiscount.removeDiscount(v);
+                    DiscountComponent d = dbdiscount.getDiscountByID(v.getId());
+                    Assert.IsTrue(d==null);
+
+                }
+                catch (Exception)
+                {
+                    Assert.Fail();
+                }
+
+            }
+            [TestMethod]
+            public void AllStoreDiscounts()
+            {
+                try
+                {
+                    VisibleDiscount v = new VisibleDiscount(0.2, "12/12/2020", "StoreVisibleDiscount", store2.getStoreID());
+                    ReliantDiscount r = new ReliantDiscount(0.2, "12/12/2020", 100, store2.getStoreID());
+                    dbdiscount.addDiscount(v);
+                    dbdiscount.addDiscount(r);
+                    LinkedList<DiscountComponent> list = dbdiscount.getStoreDiscountsList(store2.getStoreID());
+                    Assert.IsTrue(true);
 
                 }
                 catch (Exception)
